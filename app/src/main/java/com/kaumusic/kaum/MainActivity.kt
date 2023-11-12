f739b81442e389d90c28f5459570449c36fbf4d0
Youngjun Byun <forestwoodleaves@gmail.com>
2023-11-12 17:34:13 +0900

    more

--- app/src/main/java/com/kaumusic/kaum/MainActivity.kt
+++ app/src/main/java/com/kaumusic/kaum/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.kaumusic.kaum.Song
 import com.kaumusic.kaum.SongActivity
 import com.kaumusic.kaum.databinding.ActivityMainBinding
 
@@ -11,16 +12,27 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private var song:Song = Song()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.mainPlayerCl.setOnClickListener { // main player를 누르면 Activity가 넘어간다
             // intent : 하나의 activity에서 다른 activity로 전환할때 사용한다
-            startActivity(Intent(this, SongActivity::class.java))
+            val editor = getSharedPreferences("song", MODE_PRIVATE).edit()
+            editor.putInt("songId",song.id)
+            editor.apply()
 
+            val intent = Intent(this,SongActivity::class.java)
+            startActivity(intent)
         }
         initBottomNavigation()
+
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString(),0,60,false)
+
+        Log.d("Song", song.title + song.singer) // 빠르게 해당 Log를 찾아서 디버깅하기 위한 용도
     }
 
     private fun initBottomNavigation(){
