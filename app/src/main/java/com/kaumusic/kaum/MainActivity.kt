5d368e9589dd7e690a34d455a303914ab0514d39
Youngjun Byun <forestwoodleaves@gmail.com>
2023-11-17 20:53:15 +0900

    5주차 강의까지 진행완료
    -HomeFragment 오류 이유 : homeAlbumImgIv1이 최종본 XML 상에서는 주석처리 되어 Comment Out 되어있는데 아직 5주차까진 코드가 그대로 이어짐
    -MainActivity 오류 이유 : Bottom Bar Navigation 파일이 아직 강의상에서 구현이 안되어 일단 빼놓음

--- app/src/main/java/com/kaumusic/kaum/MainActivity.kt
+++ app/src/main/java/com/kaumusic/kaum/MainActivity.kt
@@ -4,28 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.gson.Gson
 import com.kaumusic.kaum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private var song:Song = Song()
+    private var gson: Gson = Gson()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FLO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString())
+
+        initBottomNavigation()
+
 
         binding.mainPlayerCl.setOnClickListener {
             val intent = Intent(this,SongActivity::class.java)
             intent.putExtra("title", song.title)
             intent.putExtra("singer", song.singer)
+            intent.putExtra("second", song.second)
+            intent.putExtra("playTime", song.playTime)
+            intent.putExtra("isPlaying", song.isPlaying)
+            intent.putExtra("music", song.music)
             startActivity(intent)
         }
-        initBottomNavigation()
-
-
-        Log.d("Song", song.title + song.singer)
 
     }
 
@@ -67,5 +73,27 @@ class MainActivity : AppCompatActivity() {
             false
         }
     }
+
+    private fun setMiniPlayer(song : Song){
+        binding.mainMiniplayerTitleTv.text = song.title
+        binding.mainMiniplayerSingerTv.text = song.singer
+        binding.mainMiniplayerProgressSb.progress = (song.second*100000)/song.playTime
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
+        val songJson = sharedPreferences.getString("songData", null)
+
+        song = if(songJson == null){
+            Song("라일락", "아이유(IU)", 0,60, false, "music_lilac")
+        } else {
+            gson.fromJson(songJson, Song::class.java)
+        }
+        setMiniPlayer(song)
+
+
+    }
+
 
 }
--- app/src/main/java/com/kaumusic/kaum/Song.kt
+++ app/src/main/java/com/kaumusic/kaum/Song.kt
@@ -1,6 +1,10 @@
 package com.kaumusic.kaum
 
 data class Song(
-    val title : String = "",
-    val singer : String = ""
+    var title: String = "",
+    var singer: String = "",
+    var second: Int = 0,
+    var playTime: Int = 0,
+    var isPlaying: Boolean = false,
+    var music: String = ""
 )
