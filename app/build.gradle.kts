9c7087c84f4f29c9e68a73105028e90340a2ac63
Youngjun Byun <forestwoodleaves@gmail.com>
2023-11-28 01:35:57 +0900

    Revert "#1 프로젝트 아키텍쳐 구조 수정"
    This reverts commit 8b0563f356b0f5e0c68acbd92b99aa56f3501176.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android" )
+    kotlin("kapt")
+//    id("com.google.devtools.ksp")
 }
 
 android {
@@ -45,6 +47,19 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.android.support:support-annotations:28.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
+
+    // roomDB
+
+    val room_version = "2.5.0"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+//    ksp("androidx.room:room-compiler:$room_version")
+
 
 
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/kaumusic/kaum/model/entity/Album.kt
+++
@@ -1,8 +0,0 @@
-package com.kaumusic.kaum.model.entity
-
-data class Album(
-    var title: String? = "",
-    var singer: String? = "",
-    var coverImg: Int? = null,
-    var songs: ArrayList<Song>? = null
-)
